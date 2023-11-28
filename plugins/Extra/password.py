de7a5b7e8182f1945119644490db528820907990
TOICTONY <114583077+TOICTONY@users.noreply.github.com>
2023-11-28 21:57:35 +0530

    Update password.py

--- plugins/Extra/password.py
+++ plugins/Extra/password.py
@@ -19,5 +19,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('TELUGUZONE', url='https://t.me/teluguzoneofc')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('MoviiWrld', url='https://t.me/MoviiWrld')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
