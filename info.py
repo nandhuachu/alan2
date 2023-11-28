a10a828a7e90339885c4697b4caf9cdb6c81d3db
TOICTONY <114583077+TOICTONY@users.noreply.github.com>
2023-11-28 21:07:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://MoviiWrld:MoviiWrld01@cluster0.lloilyc.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "MoviiWrld")
+DATABASE_NAME = environ.get('DATABASE_NAME', "MoviiWrld234")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
