539804d701f6dab4337df738374d8f831ce75471
TOICTONY <114583077+TOICTONY@users.noreply.github.com>
2024-02-02 20:41:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,12 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TOICTONY/render
+  git clone https://github.com/TOICTONY/render.git /MW-MOVIE-BOT 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /MW_MoViE_BOT
+  git clone $UPSTREAM_REPO /MW-MOVIE-BOT 
 fi
-cd /MW_MoViE_BOT 
+cd /MW-MOVIE-BOT 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
