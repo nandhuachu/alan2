c3b5c040589290905c200054c01b868ad00aca3f
Venom <67759177+nandhuachu@users.noreply.github.com>
2024-03-23 23:24:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TOICTONY/render.git /MW-MOVIE-BOT 
+  git clone https://github.com/nandhuachu/alan2.git /alan2 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /MW-MOVIE-BOT 
+  git clone $UPSTREAM_REPO /alan2 
 fi
-cd /MW-MOVIE-BOT 
+cd /alan2 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
