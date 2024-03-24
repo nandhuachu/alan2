db271f3acd23d5b28c9780f8c2adbfc14f3b0577
Venom <67759177+nandhuachu@users.noreply.github.com>
2024-03-24 10:43:07 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/nandhuachu/alan2.git /alan2
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /alan2
+fi
+cd /alan2
+pip3 install -U -r requirements.txt
+echo "Starting...."
+python3 bot.py
